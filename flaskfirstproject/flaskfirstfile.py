d39bef319951f9f7002117d15efd29b1125f4b90
M.Usman <et_dev43@theentertainerme.com>
2020-01-21 15:55:33 +0500

    Added theme,css

--- flaskfirstproject/flaskfirstfile.py
+++ flaskfirstproject/flaskfirstfile.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , url_for
 app = Flask(__name__)
 
 
@@ -12,7 +12,7 @@ posts=[
 	{
 		"author":"Muhammad Usman",
 		"title":"Blog post 2",
-		"content":"second Blog Post",
+		"content":"Second Blog Post",
 		"date_posted":"April 21, 2017"
 	}
 ]
