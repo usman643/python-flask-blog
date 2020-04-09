c692fa129e28123988beb5cd7e18c512df527526
Muhammad Usman <et_dev43@theentertainerme.com>
2020-04-09 11:20:53 +0500

    Login and register classes added

--- flaskfirstproject/flaskfirstfile.py
+++ flaskfirstproject/flaskfirstfile.py
@@ -1,6 +1,10 @@
-from flask import Flask , render_template , url_for
+from flask import Flask , render_template , url_for, flash , redirect
+from forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '3c983e8373f6b977c97e67982f421314'
 
 posts=[
 	{
@@ -27,6 +31,29 @@ def home():
 def about():
 	return render_template('about.html', title='About')
 
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account Created for { form.username.data }!' , 'success')
+		return redirect(url_for('home'))
+
+
+	return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'a@a.com' and form.password.data == 'password':
+			flash(f'You have loggedIn!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash(f'Something went wrong! Please check email and password', 'danger')
+	return render_template('login.html', title='Login', form=form)
 
 
 
